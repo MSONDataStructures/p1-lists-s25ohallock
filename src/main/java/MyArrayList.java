39784cea1403b670b742ef2e5cae7ba0a1486a3d
Owen Hallock <owenhallock29@gmail.com>
2024-10-30 19:28:01 -0400

    Update MyArrayList.java

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,133 +1,167 @@
-/**
- * The MyArrayList class is the implementation of an array list of integers.
- * <p>
- * The data in the list will be stored in an array, and the basic
- * <code>ArrayList</code> methods <code>add</code>, <code>remove</code>,
- * <code>set</code>, <code>get</code>, and <code>size</code> will be
- * implemented, as well as the additional methods <code>indexOf</code>,
- * <code>contains</code>, <code>clear</code>, and <code>isEmpty</code>.
- * <p>
- * Students should not, obviously, use the <code>java.util.ArrayList</code>
- * Java library class. The choices that you make regarding the management
- * of the list array will influence how the methods are implemented. You
- * may add any additional instance fields as desired. The choice that has
- * been made for you is that the default constructor should give an initial
- * capacity of ten elements. Be sure to avoid the <code>IndexOutOfBoundsException</code>,
- * and you will need to throw the <code>NullPointerException</code> in places
- * as specified in the Javadoc and the JUnit <code>MyArrayListTest</code> class.
- */
-public class MyArrayList
-{
+public class MyArrayList {
     private Integer[] list;
-
+    private int size;
     /**
      * Constructs an empty list with an initial capacity of ten.
      */
     public MyArrayList() {
         list = new Integer[10];
-        // TODO: you can add code here
+        size = 0;
     }
-
     /**
-     * Appends the specified Integer to the <b>end</b> of the list.
+     * Appends the specified Integer to the end of the list.
      * @param item Integer to be appended to this list
      * @throws NullPointerException if item is null
      */
     public void addLast(Integer item) {
-        // TODO: your code goes here
+        if (item == null) {
+            throw new NullPointerException("Item cannot be null.");
+        }
+        if (size == list.length) {
+            int newCapacity = list.length * 2;
+            Integer[] newList = new Integer[newCapacity];
+
+            System.arraycopy(list, 0, newList, 0, list.length);
+
+            list = newList;
+        }
+
+        list[size++] = item;
     }
 
     /**
      * Inserts the specified Integer at the specified position in this list.
-     * Shifts the element currently in that position (if any) and any subsequent
-     * elements to the right (adding one to their indices).
+     * Shifts the element currently at that position (if any) and subsequent
+     * elements to the right.
      * @param index index at which the specified element is to be inserted
      * @param item Integer to be inserted
      * @throws NullPointerException if item is null
+     * @throws IndexOutOfBoundsException if index is out of range
      */
     public void add(int index, Integer item) {
-        // TODO: your code goes here
-    }
+        if (item == null) {
+            throw new NullPointerException("Item cannot be null, silly");
+        }
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        for (int i = size; i > index; i--) {
+            list[i] = list[i - 1];
+        }
 
+        list[index] = item;
+        size++;
+    }
     /**
-     * Removes the Integer at the specified position in this list. Shifts any
-     * subsequent Integers to the left (subtracts one from their indices).
+     * Removes the Integer at the specified position in this list.
+     * Shifts any subsequent elements to the left.
+     *
      * @param index the index of the element to remove
-     * @return the element that was removed from the list
+     * @throws IndexOutOfBoundsException if index is out of range
      */
-    public Integer remove(int index) {
-        // TODO: modify the code here
-        return null;
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index. Oops");
+        }
+        for (int i = index; i < size - 1; i++) {
+            list[i] = list[i + 1];
+        }
+
+        list[size - 1] = null;
+        size--;
     }
 
     /**
      * Returns the Integer at the specified position in this list.
      * @param index index of the element to return
      * @return the Integer at the specified position in this list
+     * @throws IndexOutOfBoundsException if index is out of range
      */
     public Integer get(int index) {
-        // TODO: modify the code here
-        return null;
+        return list[index];
     }
 
     /**
-     * Replaces the Integer at the specified position in this list with the
-     * specified Integer.
+     * Replaces the Integer at the specified position with the specified item.
      * @param index index of the integer to replace
      * @param item Integer to be stored at the specified position
      * @throws NullPointerException if item is null
+     * @throws IndexOutOfBoundsException if index is out of range
      */
     public void set(int index, Integer item) {
-        // TODO: your code goes here
-    }
+        if (item == null){
+            throw new NullPointerException("Item cannot be null!!");
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
 
+        list[index] = item;
+    }
     /**
      * Returns the number of Integers in this list.
      * @return the number of Integers in this list
      */
     public int size() {
-        // TODO: modify the code here
-        return 0;
+        return size;
     }
 
     /**
-     * Returns the index of the first occurrence of the specified Integer
-     * in this list, or -1 if this list does not contain the Integer.
+     * Returns the index of the first occurrence of the specified Integer,
+     * or -1 if this list does not contain the Integer.
      * @param item Integer to search for
-     * @return the index of the first occurrence of the specified Integer
-     * in this list, or -1 if this list does not contain the Integer
+     * @return the index of the first occurrence, or -1 if not found
      * @throws NullPointerException if item is null
      */
+
     public int indexOf(Integer item) {
-        // TODO: modify the code here
-        return 0;
+        if (item == null) {
+            throw new NullPointerException("The item cannot be null. Got it?");
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
-     * Returns <code>true</code> if this list contains the specified Integer.
-     * @param item Integer whose presence in this list is to be tested
+     * Returns true if this list contains the specified Integer.
+     * @param item Integer whose presence is to be tested
      * @return true if this list contains the specified element
      * @throws NullPointerException if item is null
      */
     public boolean contains(Integer item) {
-        // TODO: modify the code here
+        if (item == null) {
+            throw new NullPointerException("Sadly, item cannot be null");
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (item.equals(list[i])) {  // Safer comparison: item first
+                return true;
+            }
+        }
         return false;
     }
-
     /**
      * Removes all the elements from this list.
-     * @post the capacity of the array should not change
+     * The capacity of the array should not change.
      */
-    public void clear() {
-        // TODO: your code goes here
-    }
 
+    //NOTE: clear method is not used but is kept for assignment purposes.
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            list[i] = null;
+        }
+        size = 0;
+    }
     /**
-     * Returns <code>true</code> if this list has no elements.
-     * @return true if this list is empty
+     * Returns true if this list contains no elements.
+     * @return true if this list contains no elements
      */
     public boolean isEmpty() {
-        // TODO: modify the code here
-        return false;
+        return size == 0;
     }
 }
